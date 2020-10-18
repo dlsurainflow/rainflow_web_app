aac3b2cadd650455539c3b1139cddd84fe2f38d5
wilyfreddie <garanivan59@gmail.com>
2020-10-18 08:06:58 +0800

    created build

---
+++ build/precache-manifest.5d83cbab509e260de11a1ba08322e5d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b777111108512f6802e9daa67e14de0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e6b63445d26b8d754e5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7a0b7ce69d57724beb53",
+    "url": "/static/js/2.dc54498b.chunk.js"
+  },
+  {
+    "revision": "7f3e92ce52d44bd3b516ece6d9cbc8f7",
+    "url": "/static/js/2.dc54498b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e6b63445d26b8d754e5",
+    "url": "/static/js/main.4aa926d5.chunk.js"
+  },
+  {
+    "revision": "353ab753dcd12877436a",
+    "url": "/static/js/runtime-main.c22b7962.js"
+  },
+  {
+    "revision": "7be95e035f8c20a675f1fe67ea145dc5",
+    "url": "/static/media/rainflow_logo.7be95e03.png"
+  }
+]);
